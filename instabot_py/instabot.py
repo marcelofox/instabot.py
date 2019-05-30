cf004a8df15c1d8c2b92948fb8a5f26fc36da4be
Marcelo Mendes <marcelof182@gmail.com>
2019-05-30 10:17:55 -0300

    Correçao do follow %25

--- instabot_py/instabot.py
+++ instabot_py/instabot.py
@@ -933,6 +933,7 @@ class InstaBot:
                         self.logger.info(
                             f"Won't follow {username}: user is not following more people than follow him"
                         )
+                        time.sleep(10) #Sleeps 10 seconds to avoid massive profile GET
                         return
 
                 except Exception:
